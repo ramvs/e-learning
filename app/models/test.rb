5594276e6c10ceb1f448367deb3d275ad38465e7
Grzegorz Kapica <grzk@outlook.com>
2015-04-15 21:02:04 +0200

    test-lesson one to one

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,4 @@
 class Test < ActiveRecord::Base
+	belongs_to :lesson
+	validates :lesson , presence: true , uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415170517) do
+ActiveRecord::Schema.define(version: 20150415185500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,10 @@ ActiveRecord::Schema.define(version: 20150415170517) do
     t.datetime "updated_at",  null: false
     t.string   "title"
     t.string   "description"
+    t.integer  "lesson_id",   null: false
   end
+
+  add_index "tests", ["lesson_id"], name: "index_tests_on_lesson_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -57,4 +60,5 @@ ActiveRecord::Schema.define(version: 20150415170517) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "tests", "lessons"
 end
